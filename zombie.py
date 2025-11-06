788e302f64e4f4435a276f49cc53f94311496312
hyeon025 <kjh04255@naver.com>
2025-11-06 22:33:38 +0900

    좀비가 공에 맞은 횟수 세는 변수 추가

--- zombie.py
+++ zombie.py
@@ -33,6 +33,7 @@ class Zombie:
         self.load_images()
         self.frame = random.randint(0, 9)
         self.dir = random.choice([-1,1])
+        self.hit_count = 0
 
 
     def get_bb(self):
